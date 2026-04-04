5fa76e9e518bb3faa316b72343a4eabb8ab20655
fraol163 <fraolteshome123@gmail.com>
2026-04-05 01:34:33 +0300

    fixed 9

--- src/components/gebeta/GebetaTab.tsx
+++ src/components/gebeta/GebetaTab.tsx
@@ -344,7 +344,7 @@ export default function GebetaTab() {
       </div>
 
       {/* AI Recommendation */}
-      <div className="px-4 md:px-8 py-6 relative z-10">
+      <div className="px-4 md:px-8 mt-8 relative z-10">
         <div className="max-w-3xl mx-auto">
         {!aiPick ? (
           <button
